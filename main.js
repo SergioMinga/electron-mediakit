8565a37a7b6ea24ef79e930a17744d0d49ffc9ad
Checho <sergio@mingadigital.com>
2020-02-04 15:06:02 -0300

    electron bootstrap

--- main.js
+++ main.js
@@ -1,41 +1,34 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+class Main {
+
+    constructor() {
+        app.on('ready', this.createWindow)
+        app.on('window-all-closed', () => {
+            if (process.platform !== 'darwin') {
+                app.quit()
+            }
+        })
+        app.on('activate', () => {
+            if (this.browserWindow === null) {
+                this.createWindow()
+            }
+        })
     }
-  })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    createWindow() {
+        this.browserWindow = new BrowserWindow({width: 1024, height: 768, minWidth: 640, minHeight: 480})
+        this.browserWindow.loadURL(url.format({
+            pathname: path.join(__dirname, './index.html'),
+            protocol: 'file:',
+            slashes: true
+        }))
+        this.browserWindow.on('closed', () => {
+            this.browserWindow = null
+        })
+    }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+const main = new Main()
---
+++ renderer/components/OverPage.js
@@ -0,0 +1,7 @@
+module.exports = class Crud extends (require("../Component")) {
+
+    render() {
+        return `<h1>Overpage</h1>${this.renderer.settings.exampleText}`;
+    }
+
+}
